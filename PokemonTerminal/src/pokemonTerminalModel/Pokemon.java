964c4ff6ebf5ea6a1a69c6442c91d21d1f52bbf2
CarlitrosPicaTecla <ortega.recar22@triana.salesianos.edu>
2021-11-26 12:10:49 +0100

    Actualizacion clase pokemon

--- PokemonTerminal/src/pokemonTerminalModel/Pokemon.java
+++ PokemonTerminal/src/pokemonTerminalModel/Pokemon.java
@@ -3,4 +3,111 @@ package pokemonTerminalModel;
 public class Pokemon {
 
 	private int vida;
+	private int ataque;
+	private int defensa;
+	private int defensaEspecial;
+	private int especial;
+	private int velocidad;
+	private int precision;
+	private int evasion;
+	
+	
+	
+	
+	public Pokemon(int vida, int ataque, int defensa, int defensaEspecial, int especial, int velocidad, int precision,
+			int evasion) {
+		this.vida = vida;
+		this.ataque = ataque;
+		this.defensa = defensa;
+		this.defensaEspecial = defensaEspecial;
+		this.especial = especial;
+		this.velocidad = velocidad;
+		this.precision = precision;
+		this.evasion = evasion;
+	}
+
+
+	public int getVida() {
+		return vida;
+	}
+
+
+	public void setVida(int vida) {
+		this.vida = vida;
+	}
+
+
+	public int getAtaque() {
+		return ataque;
+	}
+
+
+	public void setAtaque(int ataque) {
+		this.ataque = ataque;
+	}
+
+
+	public int getDefensa() {
+		return defensa;
+	}
+
+
+	public void setDefensa(int defensa) {
+		this.defensa = defensa;
+	}
+
+
+	public int getDefensaEspecial() {
+		return defensaEspecial;
+	}
+
+
+	public void setDefensaEspecial(int defensaEspecial) {
+		this.defensaEspecial = defensaEspecial;
+	}
+
+
+	public int getEspecial() {
+		return especial;
+	}
+
+
+	public void setEspecial(int especial) {
+		this.especial = especial;
+	}
+
+
+	public int getVelocidad() {
+		return velocidad;
+	}
+
+
+	public void setVelocidad(int velocidad) {
+		this.velocidad = velocidad;
+	}
+
+
+	public int getPrecision() {
+		return precision;
+	}
+
+
+	public void setPrecision(int precision) {
+		this.precision = precision;
+	}
+
+
+	public int getEvasion() {
+		return evasion;
+	}
+
+
+	public void setEvasion(int evasion) {
+		this.evasion = evasion;
+	}
+	
+	
+	
+	
+	
 }
