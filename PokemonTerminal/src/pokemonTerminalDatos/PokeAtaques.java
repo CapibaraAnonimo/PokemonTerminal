b1d49f2f00a61593f2f828bc88748ed855a0c853
CapibaraAnonimo <adrianarnaizcano@gmail.com>
2021-11-26 19:41:10 +0100

    He continuado con los datos

--- PokemonTerminal/src/pokemonTerminalDatos/PokeAtaques.java
+++ PokemonTerminal/src/pokemonTerminalDatos/PokeAtaques.java
@@ -1,5 +1,9 @@
 package pokemonTerminalDatos;
 
 public class PokeAtaques {
+	
+	public int ataqueID[] = {};
+	
+	public String ataqueNombre[][] = {{}, {}};
 
 }
--- PokemonTerminal/src/pokemonTerminalDatos/PokeStats.java
+++ PokemonTerminal/src/pokemonTerminalDatos/PokeStats.java
@@ -2,6 +2,8 @@ package pokemonTerminalDatos;
 
 public class PokeStats {
 	//pokede num, hp, ataque, defensa, ataque especial, defensa especial, velocidad
-	public int stats[][] = {{1, 136, 49, 49, 65, 65, 45}, {}};
+	public int stats[][] = {{1, 120, 75, 69, 76, 85, 65}, //Bulbasur
+							{4, 114, 79, 63, 72, 70, 85}, //Charmander
+							{7, 119, 74, 85, 63, 84, 63}}; //Squirtle
 
 }
