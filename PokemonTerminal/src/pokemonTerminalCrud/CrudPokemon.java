8d275648d42a2567731725848a629e844fc04adf
Ana Pilar Barba López <pilarbarba03@gmail.com>
2021-11-26 12:34:29 +0100

    Empezando Vista y Crud

--- PokemonTerminal/src/pokemonTerminalCrud/CrudPokemon.java
+++ PokemonTerminal/src/pokemonTerminalCrud/CrudPokemon.java
@@ -1,5 +1,22 @@
 package pokemonTerminalCrud;
 
+import pokemonTerminalModel.Pokemon;
+
 public class CrudPokemon {
+	
+	private Pokemon pokemenon;
+	
+	//Voy a comenzar a realizar la actualización de la defensa
+	
+	//Método 1: Importo el paquete Model para llamar a los atributos y poder actualizar
+	public static void actualizarDefensa (int defensa) {
+		//get.defensa + defensa;
+		
+		
+	}
+	
+	public static void actualizarDefensaEspecial (int defensaEspecial) {
+		//get.defensaEspecial + defensaEspecial;
+	}
 
 }
