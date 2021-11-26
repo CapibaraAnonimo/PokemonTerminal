15df26c3d598830cd374c0936f5f8ff1956afbc6
Ana Pilar Barba López <pilarbarba03@gmail.com>
2021-11-26 12:37:53 +0100

    Empezando Vista y Crud

--- PokemonTerminal/src/pokemonTerminalCrud/CrudPokemon.java
+++ PokemonTerminal/src/pokemonTerminalCrud/CrudPokemon.java
@@ -8,13 +8,14 @@ public class CrudPokemon {
 	
 	//Voy a comenzar a realizar la actualización de la defensa
 	
-	//Método 1: Importo el paquete Model para llamar a los atributos y poder actualizar
+	//Método 1: Importo el paquete Model para llamar a los atributos y poder actualizar la defensa
 	public static void actualizarDefensa (int defensa) {
 		//get.defensa + defensa;
 		
 		
 	}
 	
+	//
 	public static void actualizarDefensaEspecial (int defensaEspecial) {
 		//get.defensaEspecial + defensaEspecial;
 	}
--- PokemonTerminal/src/pokemonTerminalVista/Imprimir.java
+++ PokemonTerminal/src/pokemonTerminalVista/Imprimir.java
@@ -1,5 +1,22 @@
 package pokemonTerminalVista;
 
 public class Imprimir {
+	
+	static String menuAtaqueArriba = "**************************"
+							        +"*                        *";
+	
+	static String menuAtaqueAbajo =  "*                        *"
+								   + "**************************";
+	
+	static String ataque1 = "Ataque1";
+	static String ataque2 = "Ataque2";
+	static String ataque3 = "Ataque3";
+	static String ataque4 = "Ataque4";
+	
+	
+		public static void menuMovimiento () {
+		System.out.println(menuAtaqueArriba + ataque1 + ataque2 + ataque3 + ataque4 + menuAtaqueAbajo);
+	}
+	
 
 }
