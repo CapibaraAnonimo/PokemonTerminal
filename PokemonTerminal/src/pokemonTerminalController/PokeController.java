ccb9f0e20d52341184dfe82109fc05d247c16f49
Adrián Arnaiz Cano <adrianarnaizcano@gmail.com>
2021-11-26 14:23:31 +0100

    He empezado con los datos

--- PokemonTerminal/src/pokemonTerminalController/PokeController.java
+++ PokemonTerminal/src/pokemonTerminalController/PokeController.java
@@ -4,7 +4,7 @@ public class PokeController {
 	
 	public static int calcularDaño(int nivel, int a, int d, int potencia, int critico, int random, int bmta, int efectividad)
 	{
-		return ((((((2*nivel)/5)+2)*potencia*(a/d))/50) *critico*random*bmta*efectividad); //
-	}
+		return ((((((2*nivel)/5)+2)*potencia*(a/d))/50) *critico*random*bmta*efectividad);
+	}	
 
 }
--- PokemonTerminal/src/pokemonTerminalDatos/PokeStats.java
+++ PokemonTerminal/src/pokemonTerminalDatos/PokeStats.java
@@ -1,5 +1,7 @@
 package pokemonTerminalDatos;
 
 public class PokeStats {
+	//pokede num, hp, ataque, defensa, ataque especial, defensa especial, velocidad
+	public int stats[][] = {{1, 136, 49, 49, 65, 65, 45}, {}};
 
 }
