25a724c1ede37e82f18bf3728df7aec7c019cdc5
Adrián Arnaiz Cano <adrianarnaizcano@gmail.com>
2021-11-30 13:31:53 +0100

    He mejorado el model movimiento y he añadido movimientos

--- PokemonTerminal/src/pokemonTerminalDatos/PokeAtaques.java
+++ PokemonTerminal/src/pokemonTerminalDatos/PokeAtaques.java
@@ -7,10 +7,10 @@ public class PokeAtaques {
 	public int ataqueID[] = {};
 	
 	public Movimiento ataqueNombre[] = {
-										new Movimiento(22, "Látigo Cepa", 11, 45, 100, 25, 0, 0, 0, 0, 0),
-										new Movimiento(33, "Placaje", 0, 35, 100, 35, 0, 0, 0, 0, 0),
-										new Movimiento(75, "Hoja Afilada", 11, 55, 95, 25, 0, 0, 0, 0, 0),
-										new Movimiento(76, "Rayo Solar", 11, 120, 100, 10, 1, 0, 0, 0, 0),
+										new Movimiento(22, "Látigo Cepa", 11, 45, 100, 25, 0),
+										new Movimiento(33, "Placaje", 0, 35, 100, 35, 0),
+										new Movimiento(75, "Hoja Afilada", 11, 55, 95, 25, 0),
+										new Movimiento(76, "Rayo Solar", 11, 120, 100, 10, 1),
 																								};
 
 }
