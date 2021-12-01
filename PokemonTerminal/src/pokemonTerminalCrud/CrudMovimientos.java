7a96fb111515d9312f8faaec070a410e45a62f6c
MaylorSr <maylorbustamante2001@gmail.com>
2021-12-01 10:52:22 +0100

    Se ha actualizado el crud

--- PokemonTerminal/src/pokemonTerminalCrud/CrudMovimientos.java
+++ PokemonTerminal/src/pokemonTerminalCrud/CrudMovimientos.java
@@ -3,18 +3,25 @@ package pokemonTerminalCrud;
 import pokemonTerminalModel.Movimiento;
 
 public class CrudMovimientos {
-
+	
+	//se inyecta la clase crud moviemiento
 	private Movimiento crudMovimiento;
 
+	//se crea un constructor para igualar.
 	public CrudMovimientos(Movimiento crudMovimiento) {
 		this.crudMovimiento = crudMovimiento;
 	}
 	
-	//metodo para actualizar los movimientos 
+	//metodo para actualizar los movimientos (el Pp)
 	public void actualizarMovimiento () {
 		crudMovimiento.setPp(crudMovimiento.getPp() - 1);
-			
+		
+		
 	}
 
+	
+	
+	
+	
 	
 }
--- PokemonTerminal/src/pokemonTerminalCrud/CrudPokemon.java
+++ PokemonTerminal/src/pokemonTerminalCrud/CrudPokemon.java
@@ -1,49 +1,37 @@
 package pokemonTerminalCrud;
 
 import pokemonTerminalModel.Pokemon;
-import pokemonTerminalModel.Movimiento;
 
 
 public class CrudPokemon {
-	//se crea un objeto crud el cual es CrudPokemon y esta vacío
+	//se "inyecta la clase Pokemon para acceder a los atributos de la clase Pokemon"
 	
-	private Pokemon crudPokemon;
-	private Movimiento crudMovimiento;
+	private Pokemon crudPokemon;	
 	
-	
+	//Se crea el constructor pokemon para poder igualar
 	public CrudPokemon(Pokemon crudPokemon) {
 		this.crudPokemon = crudPokemon;
 	}
 	
-	
-	//Voy a comenzar a realizar la actualizaci�n de la defensa
-	
-	//M�todo 1: Importo el paquete Model para llamar a los atributos y poder actualizar la defensa
-	//A ra�zdel ejemplo de �ngel, lo llamamos con los set y get del paquete model
+	//METODOS PARA ACTUALIZAR
 	public void actualizarDefensa (int actDefensa) {
 		crudPokemon.setDefensa(crudPokemon.getDefensa() + actDefensa);
 	}
 
-	//Continuo con la actualizaci�n de la defensa especial, que es basicamente lo mismo que el anterior a la hora de llamar los atributos
 	public void actualizarDefensaEspecial (int actDefensaEspecial) {
 		crudPokemon.setDefensaEspecial(crudPokemon.getDefensaEspecial() + actDefensaEspecial);
 	}
-	/*------------------------------------------------------------------------------------------------------------------------------------------------------------*/
-	//Maylor procedo a crear métodos para :
-	//Actualizar la vida Método 1
-	//el acVida es actualizar vida, uso un public void para que se pueda acceder a este
+
 	public void actulizarVida (int acVida) {
 		crudPokemon.setVida(crudPokemon.getVida() + acVida);
 		
 	}
 	
-	//Metodo numero 3 actualizar daño
 	public void actualizarAtaque(int acAtaque) {
 		crudPokemon.setAtaque(crudPokemon.getAtaque() + acAtaque);
 		
 	}
 	
-	//Metodo actualizar ataqueespecial
 	public void actualizarAtaqueEspecial (int acAtaqueEspecial) {
 		crudPokemon.setAtaqueEspecial(crudPokemon.getAtaqueEspecial() + acAtaqueEspecial);
 		
