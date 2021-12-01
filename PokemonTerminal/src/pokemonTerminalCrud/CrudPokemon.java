36f44c6c4218e84d000fd8f11b49bae2468a9eef
MaylorSr <maylorbustamante2001@gmail.com>
2021-12-01 09:07:30 +0100

    Se ha actualizado los movmientos

--- PokemonTerminal/src/pokemonTerminalCrud/CrudPokemon.java
+++ PokemonTerminal/src/pokemonTerminalCrud/CrudPokemon.java
@@ -1,22 +1,24 @@
 package pokemonTerminalCrud;
 
 import pokemonTerminalModel.Pokemon;
+import pokemonTerminalModel.Movimiento;
 
 
 public class CrudPokemon {
 	//se crea un objeto crud el cual es CrudPokemon y esta vacÃ­o
 	
 	private Pokemon crudPokemon;
+	private Movimiento crudMovimiento;
 	
 	//Voy a comenzar a realizar la actualizaciï¿½n de la defensa
 	
 	//Mï¿½todo 1: Importo el paquete Model para llamar a los atributos y poder actualizar la defensa
-	//A raízdel ejemplo de Ángel, lo llamamos con los set y get del paquete model
+	//A raï¿½zdel ejemplo de ï¿½ngel, lo llamamos con los set y get del paquete model
 	public void actualizarDefensa (int actDefensa) {
 		crudPokemon.setDefensa(crudPokemon.getDefensa() + actDefensa);
 	}
 
-	//Continuo con la actualización de la defensa especial, que es basicamente lo mismo que el anterior a la hora de llamar los atributos
+	//Continuo con la actualizaciï¿½n de la defensa especial, que es basicamente lo mismo que el anterior a la hora de llamar los atributos
 	public void actualizarDefensaEspecial (int actDefensaEspecial) {
 		crudPokemon.setDefensaEspecial(crudPokemon.getDefensaEspecial() + actDefensaEspecial);
 	}
@@ -39,6 +41,12 @@ public class CrudPokemon {
 	public void actualizarAtaqueEspecial (int acAtaqueEspecial) {
 		crudPokemon.setAtaqueEspecial(crudPokemon.getAtaqueEspecial() + acAtaqueEspecial);
 		
+	}
+	
+	//metodo para actualizar los movimientos 
+	public void actualizarMovimiento () {
+		crudMovimiento.setPp(crudMovimiento.getPp() - 1);
+			
 	}
 	
 	
