766ca6d6e563fa246dbbfcda140773f8f7e771b6
CapibaraAnonimo <adrianarnaizcano@gmail.com>
2021-12-01 20:33:40 +0100

    He continuado con partida

--- PokemonTerminal/src/principal/Partida.java
+++ PokemonTerminal/src/principal/Partida.java
@@ -9,10 +9,16 @@ public class Partida {
 	{
 		PokeStats lp = new PokeStats();
 		CrudPokemon poke[] = {new CrudPokemon(lp.getPokemon(1)), new CrudPokemon(lp.getPokemon(1))};
+		int turno = 0;
 		
 		do
 		{
-			
+			if(turno == 0)
+			{
+				turno = 1;
+			}else {
+				turno = 0;
+			}
 		}while(poke[1].getPokemon().getVida() > 0 && poke[2].getPokemon().getVida() > 0);
 
 	}
