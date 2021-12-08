a744d85dfe722a3943ba2ba325839faebf6b754b
Ana Pilar <pilarbarba03@gmail.com>
2021-12-08 23:53:13 +0100

    Terminada la clase CrudEquipo

--- PokemonTerminal/src/pokemonTerminalCrud/CrudEquipo.java
+++ PokemonTerminal/src/pokemonTerminalCrud/CrudEquipo.java
@@ -1,5 +1,29 @@
 package pokemonTerminalCrud;
 
+import pokemonTerminalModel.Equipo;
+
 public class CrudEquipo {
+	
+	private Equipo crudEquipo;
+	
+	public CrudEquipo (Equipo crudEquipo) {
+		this.crudEquipo = crudEquipo;
+	}
+	
+	//Continuo para relaizar el método de la actualización del equipo
+	
+	public void actualizarPosicion (int actualizarPosicion) {
+		crudEquipo.setPosicion(crudEquipo.getPosicion() + actualizarPosicion);
+	}
+	
+	public void actualizarPokemon (int actualizarPokemon){
+		crudEquipo.setPokemon(crudEquipo.getPokemon() + actualizarPokemon);
+	}
+	
+	//Termino devolviendo el objeto a esta clase
+	
+	public Equipo getEquipo (){
+		return crudEquipo;
+	}
 
 }
