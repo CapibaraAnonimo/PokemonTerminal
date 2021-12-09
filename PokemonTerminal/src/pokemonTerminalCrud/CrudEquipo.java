6789aa523e2c5a9fdf1c2fce46380b1e7fcb105b
Ana Pilar Barba López <pilarbarba03@gmail.com>
2021-12-09 10:02:52 +0100

    Nada, solo para hacer pull

--- PokemonTerminal/src/pokemonTerminalCrud/CrudEquipo.java
+++ PokemonTerminal/src/pokemonTerminalCrud/CrudEquipo.java
@@ -1,5 +1,11 @@
 package pokemonTerminalCrud;
 
+import pokemonTerminalModel.Equipo;
+
 public class CrudEquipo {
+	
+	//Primero comencemos en inyectar la clase que contiene Equipo
+	
+	private Equipo CrudEquipo;
 
 }
