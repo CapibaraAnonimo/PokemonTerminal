b371665c45fd7b417aa4730f47f7669737c5f430
Ana Pilar Barba López <pilarbarba03@gmail.com>
2021-12-09 11:39:40 +0100

    Cnstructor de la clase crudEquipo

--- PokemonTerminal/src/pokemonTerminalCrud/CrudEquipo.java
+++ PokemonTerminal/src/pokemonTerminalCrud/CrudEquipo.java
@@ -10,8 +10,29 @@ public class CrudEquipo {
 
 	private Equipo crudEquipo;
 	
+	private Equipo pokemon1;
+	
+	private Equipo pokemon2;
+	
+	private Equipo pokemon3;
+	
+	private Equipo pokemon4;
+	
+	private Equipo pokemon5;
+	
+	private Equipo pokemon6;
+	
 	public CrudEquipo (Equipo crudEquipo) {
 		this.crudEquipo = crudEquipo;
+	}
+	
+	public CrudEquipo (Equipo pokemon1, Equipo pokemon2, Equipo pokemon3, Equipo pokemon4, Equipo pokemon5, Equipo pokemon6) {
+		this.pokemon1 = pokemon1;
+		this.pokemon2 = pokemon2;
+		this.pokemon3 = pokemon3;
+		this.pokemon4 = pokemon4;
+		this.pokemon5 = pokemon5;
+		this.pokemon6 = pokemon6;
 	}
 	
 	//Continuo para relaizar el método de la actualización del equipo
