a77477c6d22b845a764b2e124ece3ee803c4ae4b
Ana Pilar Barba López <pilarbarba03@gmail.com>
2021-12-13 11:34:31 +0100

    Comenzando la clase Crud Objeto con tres atributos

--- PokemonTerminal/src/pokemonTerminalCrud/CrudObjetos.java
+++ PokemonTerminal/src/pokemonTerminalCrud/CrudObjetos.java
@@ -1,15 +1,26 @@
 package pokemonTerminalCrud;
 
+import pokemonTerminalModel.Pokemon;
 import pokemonTerminalModel.Objetos;
 
 public class CrudObjetos {
 
 	
 	private Objetos objetos;
-	
 	//constructor
 	public CrudObjetos (Objetos o) {
 		objetos = new Objetos (o.getPrecio(), o.getTipo(), o.getAccion());		
+		
+	}
+	
+	public CrudObjetos (Pokemon oP) {
+		oP = new Pokemon (oP.Id(), oP.getNombre(), oP.getNivel(), oP.getTipo(), oP.getVida())
+	}
+	
+	//Comienzo a realizar la actualización de la vida con objetos
+	
+	public void sumarVida () {
+		
 		
 	}
 	
