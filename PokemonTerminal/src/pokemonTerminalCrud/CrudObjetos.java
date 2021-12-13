0dce99c1cf01bca414e68e34157953860f128cf6
MaylorSr <maylorbustamante2001@gmail.com>
2021-12-13 10:43:11 +0100

    Se ha añadido datos a la clase Crud Objetos

--- PokemonTerminal/src/pokemonTerminalCrud/CrudObjetos.java
+++ PokemonTerminal/src/pokemonTerminalCrud/CrudObjetos.java
@@ -1,19 +1,53 @@
 package pokemonTerminalCrud;
 
+import pokemonTerminalModel.Pokemon;
 import pokemonTerminalModel.Objetos;
 
 public class CrudObjetos {
 
 	
 	private Objetos objetos;
+	private Pokemon oP;
 	
-	//constructor
+	//constructor objeto
 	public CrudObjetos (Objetos o) {
 		objetos = new Objetos (o.getPrecio(), o.getTipo(), o.getAccion());		
-		
+	}
+	
+	//constructor para objeto pokemon
+	public CrudObjetos(Pokemon oP) {
+		oP = new Pokemon(oP.getId(), oP.getNombre(),oP.getTipo(), oP.getVida(), oP.getAtaque(),
+				oP.getDefensa(), oP.getDefensaEspecial(), oP.getAtaqueEspecial(), oP.getVelocidad());
 	}
 	
 	//metodos para actualizar, debido a las acciones 
+	//metodo para actualizar/ acciones que hace cada objeto
+	
+	//se debe imponer condiciones para su funcionamiento
+	
+	public void accionAtaqueEspecial() {
+		oP.setAtaqueEspecial(objetos.getAccion() + oP.getAtaqueEspecial());
+		
+	}
+
+	public void accionDefensa() {
+		oP.setDefensa(objetos.getAccion() + oP.getDefensa());
+		
+	}
+	
+	public void accionDefensaEs() {
+		oP.setDefensaEspecial(objetos.getAccion() + oP.getDefensaEspecial());
+	}
+	
+	public void accionVelocidad () {
+		oP.setVelocidad(objetos.getAccion() + oP.getVelocidad());
+	}
+	
+	public void accionPresicion () {
+		oP.setPrecision(objetos.getAccion() + oP.getPrecision());
+	}
+
+	
 	
 	
 	
