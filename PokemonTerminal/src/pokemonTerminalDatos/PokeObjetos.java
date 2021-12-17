886e9aaa7e5131b61bb18e20acb3cead4844171b
CarlitrosPicaTecla <ortega.recar22@triana.salesianos.edu>
2021-12-17 10:34:52 +0100

    id para obejetos y nombre

--- PokemonTerminal/src/pokemonTerminalDatos/PokeObjetos.java
+++ PokemonTerminal/src/pokemonTerminalDatos/PokeObjetos.java
@@ -1,11 +1,21 @@
 package pokemonTerminalDatos;
 
 import pokemonTerminalModel.Objetos;
+import pokemonTerminalModel.Pokemon;
 
 public class PokeObjetos {
 
+	private Pokemon ob;
 	
 	public Objetos obj[]= {
+			
+							//id, nombre, precio, tipo, accion
+							new Objetos(1, "Poción", 300, 1, 20),
+							new Objetos(2, "SuperPoción", 700, 1, 50),
+							new Objetos(3, "HiperPoción",1200, 1, 200),
+							new Objetos(4, "Revivir", 1500, 2, 50),
+							new Objetos(5, "Revivir Max", ),
+			
 			
 	};
 }
--- PokemonTerminal/src/pokemonTerminalModel/Objetos.java
+++ PokemonTerminal/src/pokemonTerminalModel/Objetos.java
@@ -28,6 +28,20 @@ public class Objetos {
 
 	
 	
+	public String getNombre() {
+		return nombre;
+	}
+
+
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+
+
 	public int getId() {
 		return id;
 	}
