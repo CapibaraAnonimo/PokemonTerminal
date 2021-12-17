ce12b05bc4067e651e19c7ce2fe9d7a502e70e1f
CarlitrosPicaTecla <ortega.recar22@triana.salesianos.edu>
2021-12-17 13:57:35 +0100

    actualizar tienda

--- PokemonTerminal/src/pokemonTerminalCrud/CrudTienda.java
+++ PokemonTerminal/src/pokemonTerminalCrud/CrudTienda.java
@@ -1,7 +1,13 @@
 package pokemonTerminalCrud;
 
+import pokemonTerminalModel.Objetos;
+
 public class CrudTienda {
 
-	
-	
+	private CrudTienda tienda;
+	private CrudObjetos objetos;
+		
+	public void actualizarCantidad(Objetos obj, int cant) {
+		obj.setCantidad(obj.getCantidad()+cant);
+	}
 }
