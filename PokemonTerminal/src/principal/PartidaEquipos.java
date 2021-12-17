ca5200d79c5edecc3c5f66eb6278e41691423d6a
Adrián Arnaiz Cano <adrianarnaizcano@gmail.com>
2021-12-17 11:37:42 +0100

    He hecho el crudEquipo, mejorado el equipo y empezado con partida con equipos

---
+++ PokemonTerminal/src/principal/PartidaEquipos.java
@@ -0,0 +1,23 @@
+package principal;
+
+import pokemonTerminalCrud.CrudEquipo;
+import pokemonTerminalDatos.PokeAtaques;
+import pokemonTerminalDatos.PokeStats;
+import pokemonTerminalVista.Imprimir;
+
+public class PartidaEquipos {
+
+	public static void main(String[] args) 
+	{
+		PokeStats lp = new PokeStats();
+		PokeAtaques ata = new PokeAtaques();
+		Imprimir imprimir = new Imprimir();
+		
+		CrudEquipo equipos[] = new CrudEquipo[2];
+		
+		System.out.println("Jugador 1, diga su nombre o nombre de equipo");
+		
+
+	}
+
+}
