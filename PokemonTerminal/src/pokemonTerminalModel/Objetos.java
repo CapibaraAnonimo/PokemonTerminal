76fe267b2db37edd453e22ee53f9235421674b84
CarlitrosPicaTecla <ortega.recar22@triana.salesianos.edu>
2021-12-17 12:21:31 +0100

    Clase tienda actualizada

--- PokemonTerminal/src/pokemonTerminalModel/Objetos.java
+++ PokemonTerminal/src/pokemonTerminalModel/Objetos.java
@@ -3,21 +3,29 @@ package pokemonTerminalModel;
 public class Objetos {
 
 	int id;
+	int cantidad;
 	String nombre;
 	int precio;
 	int tipo;//1.Curacion(Ana), 2.Revivir, 3.ataque, 4.ataqueEsp, 5.Defensa, 6.DefensaEsp(Ana), 7.Velocidad(Ana), 8.Precision(Ana)
 	int accion;
 	
 
-	public Objetos(int id, String nombre ,int precio, int tipo, int accion) {
+	public Objetos(int id, String nombre ,int precio, int tipo, int accion, int cantidad) {
 		this.id= id;
 		this.nombre = nombre;
 		this.precio = precio;
 		this.tipo = tipo;
 		this.accion = accion;
+		this.cantidad = cantidad;
 	}
 
-
+	public int getCantidad() {
+		return cantidad;
+	}
+	
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
 	
 	
 	public String getNombre() {
