15a65e87ca226ab899d2e945a9dae5763d757538
CarlitrosPicaTecla <ortega.recar22@triana.salesianos.edu>
2021-12-17 11:38:10 +0100

    objetos nuevos

--- PokemonTerminal/src/pokemonTerminalDatos/PokeObjetos.java
+++ PokemonTerminal/src/pokemonTerminalDatos/PokeObjetos.java
@@ -1,11 +1,9 @@
 package pokemonTerminalDatos;
 
 import pokemonTerminalModel.Objetos;
-import pokemonTerminalModel.Pokemon;
 
 public class PokeObjetos {
 
-	private Pokemon ob;
 	
 	public Objetos obj[]= {
 			
@@ -14,8 +12,24 @@ public class PokeObjetos {
 							new Objetos(2, "SuperPoción", 700, 1, 50),
 							new Objetos(3, "HiperPoción",1200, 1, 200),
 							new Objetos(4, "Revivir", 1500, 2, 50),
-							new Objetos(5, "Revivir Max", ),
-			
-			
+							new Objetos(5, "Revivir Max", 2000, 2, 100),
+							new Objetos(6, "Ataque X", 500, 3, 20),
+							new Objetos(7, "Especial X", 350, 4, 20),
+							new Objetos(8, "Defensa X", 550, 5, 20),
+							new Objetos(9, "Defensa Esp X", 350, 6, 20),
+							new Objetos(10, "Velocidad X", 350, 7, 20),
+							new Objetos(11, "Precision X", 950, 8, 20),
 	};
+	
+	public Objetos getObjetos(int id){
+		for(int i = 0; i < obj.length; i++)
+		{
+			if(obj[i].getId() == id)
+			{
+				return obj[i];
+			}
+		}
+		return null;
+	}
+	
 }
