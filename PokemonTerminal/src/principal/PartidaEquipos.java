6f77f6ffd3b70bf8d971025dc8212f43b642e65c
Adrián Arnaiz Cano <adrianarnaizcano@gmail.com>
2021-12-22 10:33:41 +0100

    Comentarios en main

--- PokemonTerminal/src/principal/PartidaEquipos.java
+++ PokemonTerminal/src/principal/PartidaEquipos.java
@@ -8,6 +8,16 @@ import utilidades.Leer;
 
 public class PartidaEquipos {
 
+	/*Partes:
+	  Adrían: Main, un modelo y un crud, imprimir sprites, controllers
+	  Ana Pilar: la mitad de imprimir, la mitad de crud, la mayoría de los sprites
+	  Carlos: la mayoria de Models, todos los movimientos de datos
+	  Maylor: la mitad de imprimir, la mitad de crud, añadido las stats de los pokemon*/
+	
+	/*Al ejecutar la partida se te pide nombre y 6 pokemons, aunque todavía no se puede cambiar de pokemons en 
+	  la partida, en la tienda se pueden hacer las compras y pagar, pero no se pueden usar todavía en partida,
+	  aunque está hecho el metodo para ello; una vez en la partida se puede atacar pero algunos ataques no hacen
+	  nunca daño, tengo que ver por qué aún*/
 	public static void main(String[] args) throws InterruptedException 
 	{
 		CrudEquipo equipos[] = new CrudEquipo[2];
