bd53464cac1111d00a894cbcc7ea035181851054
Adrián Arnaiz Cano <adrianarnaizcano@gmail.com>
2021-12-22 09:20:48 +0100

    Mejoras esteticas

--- PokemonTerminal/src/pokemonTerminalController/ControllerEquipo.java
+++ PokemonTerminal/src/pokemonTerminalController/ControllerEquipo.java
@@ -7,7 +7,7 @@ import utilidades.Leer;
 
 public class ControllerEquipo {
 	
-	public static void elegirEquipo(CrudEquipo equipo) {
+	public static void elegirEquipo(CrudEquipo equipo) throws InterruptedException {
 		PokeStats lp = new PokeStats();
 		
 
@@ -19,11 +19,13 @@ public class ControllerEquipo {
 				System.out.println(lp.stats[j]);
 			}
 			System.out.printf("Seleccione el pokemon (id pokedex nacional) %d\n", (i+1));
+			Thread.sleep(1200);
 			equipo.actualizarPokemon(new CrudPokemon(lp.getPokemon(Leer.datoInt())), i);
 		}
 		System.out.println("Tu equipo es: \n");
 		for(int i = 0; i < equipo.getEquipo().getPokemons().length; i++)
 		{
+			Thread.sleep(1200);
 			System.out.println(equipo.getEquipo().getPokemon(i).getPokemon());
 		}
 		
