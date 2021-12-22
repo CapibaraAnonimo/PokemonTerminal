20be1cd31db207f9caebe253ca9787339801285d
Ana Pilar Barba López <pilarbarba03@gmail.com>
2021-12-22 08:32:53 +0100

    Realizado restar objeto en crud Objeto

--- PokemonTerminal/src/pokemonTerminalCrud/CrudObjetos.java
+++ PokemonTerminal/src/pokemonTerminalCrud/CrudObjetos.java
@@ -27,20 +27,28 @@ public class CrudObjetos {
 	public void accionObjeto(Pokemon p) {
 		if (objetos.getTipo() == 1 && p.getVida() > 0) {
 			p.setVida(objetos.getAccion() + p.getVida());
+			actualizarCantidad(-1);
 		} else if (objetos.getTipo() == 2 && p.getVida() <= 0) {
 			p.setVida(p.getVida0() / objetos.getAccion());
+			actualizarCantidad(-1);
 		} else if (objetos.getTipo() == 3 && p.getVida() > 0) {
 			p.setAtaque(objetos.getAccion() + p.getAtaque());
+			actualizarCantidad(-1);
 		} else if (objetos.getTipo() == 4 && p.getVida() > 0) {
 			p.setAtaqueEspecial(objetos.getAccion() + p.getAtaqueEspecial());
+			actualizarCantidad(-1);
 		} else if (objetos.getTipo() == 5 && p.getVida() > 0) {
 			p.setDefensa(objetos.getAccion() + p.getDefensa());
+			actualizarCantidad(-1);
 		} else if (objetos.getTipo() == 6 && p.getVida() > 0) {
 			p.setDefensaEspecial(objetos.getAccion() + p.getDefensaEspecial());
+			actualizarCantidad(-1);
 		} else if (objetos.getTipo() == 7 && p.getVida() > 0) {
 			p.setVelocidad(objetos.getAccion() + p.getVelocidad());
+			actualizarCantidad(-1);
 		} else if (objetos.getTipo() == 8 && p.getVida() > 0) {
 			p.setPrecision(objetos.getAccion() + p.getPrecision());
+			actualizarCantidad(-1);
 		}
 	}
 
