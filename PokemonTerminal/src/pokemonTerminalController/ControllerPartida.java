ceb867dd95336210033cdf434920ec6ea35d7dc2
Adrián Arnaiz Cano <adrianarnaizcano@gmail.com>
2021-12-22 10:44:33 +0100

    arreglado el mensaje de final de partida

--- PokemonTerminal/src/pokemonTerminalController/ControllerPartida.java
+++ PokemonTerminal/src/pokemonTerminalController/ControllerPartida.java
@@ -49,7 +49,7 @@ public class ControllerPartida {
 				atacante = 1;
 			}
 		}while(equipos[atacante].getEquipo().getPokemon(equipos[atacante].getEquipo().getPosicion()).getPokemon().getVida() > 0 && equipos[defensor].getEquipo().getPokemon(equipos[defensor].getEquipo().getPosicion()).getPokemon().getVida() > 0);
-		if(equipos[atacante].getEquipo().getPokemon(equipos[atacante].getEquipo().getPosicion()).getPokemon().getVida() > 0)
+		if(equipos[atacante].getEquipo().getPokemon(equipos[atacante].getEquipo().getPosicion()).getPokemon().getVida() < 0)
 		{
 			System.out.println("Ha ganado " + equipos[defensor].getEquipo().getNombreEquipo());
 		} else
